d075c6493897ab15b2162111cd88a3e2c0b3c92c
Ruben Bonil <rbonil@alcinformatica.com>
2020-01-23 19:21:25 +0100

    Test commit

--- GitFlowExample/Program.cs
+++ GitFlowExample/Program.cs
@@ -6,7 +6,7 @@ namespace GitFlowExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
