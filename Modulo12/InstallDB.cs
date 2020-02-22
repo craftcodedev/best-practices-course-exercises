2c9219c9a2f374d77eba579382f51f38fcf932f0
Blashak <blasshak@gmail.com>
2020-02-22 09:59:29 +0100

    update chuletas2

--- Modulo12/InstallDB.cs
+++ Modulo12/InstallDB.cs
@@ -1,21 +1,10 @@
 
-Install-Package MySql.Data.EntityFrameworkCore
+//1. Install mysql package
+//Install-Package MySql.Data.EntityFrameworkCore
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // get the configuration from the app settings
-            var config = new ConfigurationBuilder()
-                .SetBasePath(_env.ContentRootPath)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            // define the database to use
-            //optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
-
-            optionsBuilder.UseMySQL(config.GetConnectionString("DefaultConnection"));
-
-        }
-
-  "ConnectionStrings": {
-    "DefaultConnection": "server=localhost;database=library;user=user;password=password"
-  },
+//2. add the following content to appsettings.json
+/**
+"ConnectionStrings": {
+  "DefaultConnection": "server=localhost;database=library;user=user;password=password"
+},
+**/
