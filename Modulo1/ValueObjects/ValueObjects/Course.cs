0a108dcbb99003e6d196a9b7bed87dd295c88f06
Ruben Bonil <rbonil@alcinformatica.com>
2020-04-19 11:53:11 +0200

    Cambios en el ejercicio de ValueObjects y en los test doubles del módulo 2

---
+++ Modulo1/ValueObjects/ValueObjects/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValueObjects
+{
+    public class Course
+    {
+        private string name;
+
+        public Course(string name)
+        {
+            if (name == "")
+            {
+                throw new System.Exception("Invalid name");
+            }
+
+            this.name = name;
+        }
+
+        public string Name()
+        {
+            return this.name;
+        }
+
+        public string Description()
+        {
+            this.name = "You will learn " + this.name;
+            return this.name;
+        }
+    }
+}
---
+++ Modulo1/ValueObjects/ValueObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValueObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
