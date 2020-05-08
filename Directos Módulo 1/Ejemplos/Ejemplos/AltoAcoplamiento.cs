80a25f60ca4f68fc19f3ec34d02ae61706ef9a6d
rbonil <rbonil@alcinformatica.com>
2020-05-08 13:28:02 +0200

    Añadir ejercicios directos módulo 1

---
+++ Directos Módulo 1/Ejemplos/Ejemplos/AltoAcoplamiento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplos
+{
+    class ClassC
+    {
+        private bool _elementA;
+
+        public int MethodA()
+        {
+            if (_elementA)
+                return new ClassD()._elementB;
+
+            return 0;
+        }
+
+        public void PrintValues()
+        {
+            new ClassD().MethodB();
+        }
+    }
+
+    class ClassD
+    {
+        public int _elementB;
+
+        public void MethodB()
+        {
+            Console.WriteLine(_elementB);
+        }
+    }
+}
---
+++ Directos Módulo 1/Ejemplos/Ejemplos/BajoAcoplamiento.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplos
+{
+    class ClassA
+    {
+        private readonly ISomeInterface _interfaceImpl;
+
+        public ClassA(ISomeInterface interfaceImpl)
+        {
+            _interfaceImpl = interfaceImpl;
+        }
+
+        public int MethodA()
+        {
+            return _interfaceImpl.MethodB();
+        }
+
+        public void PrintValues()
+        {
+            _interfaceImpl.PrintValues();
+        }
+    }
+
+    interface ISomeInterface
+    {
+        int MethodB();
+        void PrintValues();
+    }
+
+    class ClassB : ISomeInterface
+    {
+        private int _elementB = 2 + 2;
+
+        public int MethodB()
+        {
+            return _elementB;
+        }
+
+        public void PrintValues()
+        {
+            Console.WriteLine(_elementB);
+        }
+    }
+}
