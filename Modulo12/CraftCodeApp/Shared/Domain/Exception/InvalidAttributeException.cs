a9f5c1ede66256c7dd711a67e0949603917fe21f
Agustín Castro Barboza <Blashak@Agustins-MacBook-Pro.local>
2020-07-05 16:51:25 +0200

    fix bug

--- Modulo12/CraftCodeApp/Shared/Domain/Exception/InvalidAttributeException.cs
+++ Modulo12/CraftCodeApp/Shared/Domain/Exception/InvalidAttributeException.cs
@@ -5,29 +5,29 @@ namespace CraftCodeApp.src.Shared.Domain.Exception
     {
         public InvalidAttributeException(string message) : base(message) { }
 
-        public static InvalidUUIDException FromText(string text)
+        public static InvalidAttributeException FromText(string text)
         {
-            return new InvalidUUIDException(text);
+            return new InvalidAttributeException(text);
         }
 
-        public static InvalidUUIDException FromEmpty(string attribute)
+        public static InvalidAttributeException FromEmpty(string attribute)
         {
-            return new InvalidUUIDException(string.Format("the {0} must not be empty", attribute));
+            return new InvalidAttributeException(string.Format("the {0} must not be empty", attribute));
         }
 
-        public static InvalidUUIDException FromValue(string attribute, string value)
+        public static InvalidAttributeException FromValue(string attribute, string value)
         {
-            return new InvalidUUIDException(string.Format("the {0} is invalid because of its value {1}", attribute, value));
+            return new InvalidAttributeException(string.Format("the {0} is invalid because of its value {1}", attribute, value));
         }
 
-        public static InvalidUUIDException FromMaxLength(string attribute, int length)
+        public static InvalidAttributeException FromMaxLength(string attribute, int length)
         {
-            return new InvalidUUIDException(string.Format("length {0} cannot be more than {1} characters.", attribute, length.ToString()));
+            return new InvalidAttributeException(string.Format("length {0} cannot be more than {1} characters.", attribute, length.ToString()));
         }
 
-        public static InvalidUUIDException FromMinLength(string attribute, int length)
+        public static InvalidAttributeException FromMinLength(string attribute, int length)
         {
-            return new InvalidUUIDException(string.Format("length {0} cannot be less than {1} characters.", attribute, length.ToString()));
+            return new InvalidAttributeException(string.Format("length {0} cannot be less than {1} characters.", attribute, length.ToString()));
         }
     }
 }
