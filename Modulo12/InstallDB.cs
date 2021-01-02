c23cbd273d8d612c37d9704c6973a92925a57f3f
craftcodedev <53405114+craftcodedev@users.noreply.github.com>
2021-01-02 19:09:49 +0100

    Update InstallDB.cs

--- Modulo12/InstallDB.cs
+++ Modulo12/InstallDB.cs
@@ -1,10 +1,33 @@
 
 //1. Install mysql package
-//Install-Package MySql.Data.EntityFrameworkCore
+//Pomelo.EntityFrameworkCore.Mysql  Version 2.1.0
 
-//2. add the following content to appsettings.json
+//2. Create dbcontext
+using Microsoft.EntityFrameworkCore;
+
+namespace elearning.Shared.Infrastructure.Persistence.Context
+{
+    public class ELearningContext : DbContext
+    {
+        public ELearningContext(DbContextOptions opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
+//3. add the following content to startup.json
+services.AddDbContextPool<ELearningContext>(options => options
+    .UseMySql(this.configuration.GetConnectionString("DefaultConnection"))
+    .EnableSensitiveDataLogging()
+);
+services.AddScoped<ELearningContext>();
+
+//4. add the following content to appsettings.json
 /**
 "ConnectionStrings": {
-  "DefaultConnection": "server=localhost;database=library;user=user;password=password"
+  "DefaultConnection": "server=localhost;database=elearning;user=root;password=root"
 },
 **/
