39870cf27d76b45ceb15bd55407875f171b3f092
craftcodedev <53405114+craftcodedev@users.noreply.github.com>
2021-01-02 19:03:05 +0100

    Update MYSQLprovider.cs

--- Modulo12/MYSQLprovider.cs
+++ Modulo12/MYSQLprovider.cs
@@ -3,6 +3,24 @@
 //Pomelo.EntityFrameworkCore.Mysql  Version 2.1.0
 
 //2. add the following content to startup.json
-services.AddDbContextPool<BlockbusterContext>(options => options
-    .UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+services.AddDbContextPool<ELearningContext>(options => options
+    .UseMySql(this.configuration.GetConnectionString("DefaultConnection"))
+    .EnableSensitiveDataLogging()
 );
+services.AddScoped<ELearningContext>();
+
+//3. create dbcontext
+using Microsoft.EntityFrameworkCore;
+
+namespace elearning.Shared.Infrastructure.Persistence.Context
+{
+    public class ELearningContext : DbContext
+    {
+        public ELearningContext(DbContextOptions opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
